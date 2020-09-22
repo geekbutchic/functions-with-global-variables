4698b02a0538613c47b2e12d158597159bec6f65
sonny valenzuela <sonnyleevalenz@icloud.com>
2020-09-22 09:43:27 -0400

    finished all challenges

--- main.test.js
+++ main.test.js
@@ -2,10 +2,37 @@
  * YOUR CODE GOES HERE THIS TIME! *
  **********************************/
 
+let x = 3;
+
+function double () { 
+  x = x * 2;
+}
+
+let y = 10;
+
+function triple () {
+  y = y * 3;
+}
+
+let z = 4
+
+function square () {
+  z = z * z; 
+}
+
+let greeting = "Hello";
+let firstName = "Sonny";
+
+function beNice () {
+  greeting = "hello" + "Sonny"
+}
+
+function changeName () {
+  firstName = "call ended";
+}
 
 
 
- 
 
 /*********************************************************************************************************************
 
